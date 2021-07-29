e63be92b4224a2b8833b5b109b727fb499a6ff54
Hendrik Borghorst <hendrikborghorst@gmail.com>
2021-07-29 17:54:16 +0000

    Enable panvk for Rockchip OdroidGoAdvance

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="21.2.1"
-PKG_SHA256="c09d1170630150915ed5bbadfed1394f5a6f4cdbf40fde39d4a24f605cb5b052"
+PKG_VERSION="2b4b31044de83460a218e07ddec026d29653ad0b"
+# PKG_SHA256=""
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
-PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
+PKG_URL="https://gitlab.freedesktop.org/mesa/mesa.git"
 PKG_DEPENDS_TARGET="toolchain Mako:host expat libdrm"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
@@ -39,6 +39,8 @@ if [ "$VULKAN_SUPPORT" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" $VULKAN"
   if [ "$DEVICE" = "RPi4" ]; then
     PKG_MESON_OPTS_TARGET="${PKG_MESON_OPTS_TARGET//-Dvulkan-drivers=/-Dvulkan-drivers=broadcom}"
+  elif [ "$PROJECT" = "Rockchip" ] && [ "DEVICE" = "OdroidGoAdvance" ] ; then
+    PKG_MESON_OPTS_TARGET="${PKG_MESON_OPTS_TARGET//-Dvulkan-drivers=/-Dvulkan-drivers=panvk}"
   elif [ "$PROJECT" = "Generic" ]; then
     PKG_MESON_OPTS_TARGET="${PKG_MESON_OPTS_TARGET//-Dvulkan-drivers=/-Dvulkan-drivers=amd,intel}"
   fi
