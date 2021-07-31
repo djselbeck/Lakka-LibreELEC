2ce1ec22049fd378cf37d9fc24ccd2e2f2fbc382
Hendrik Borghorst <hendrikborghorst@gmail.com>
2021-07-31 10:47:59 +0000

    libxkbcommon: Fix build

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="60ddcff932b7fd352752d51a5c4f04f3d0403230a584df9a2e0d5ed87c486c8b"
 PKG_LICENSE="MIT"
 PKG_SITE="http://xkbcommon.org"
 PKG_URL="http://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="wayland:host toolchain xkeyboard-config"
+PKG_DEPENDS_TARGET="wayland:host wayland-protocols toolchain xkeyboard-config"
 PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
 
 PKG_MESON_OPTS_TARGET="-Denable-docs=false"
